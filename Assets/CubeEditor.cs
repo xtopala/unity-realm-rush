e80226c8f0a210ac334b46cd8c695b17d518e3b1
xtopala <xtopala@gmail.com>
2020-11-09 18:30:35 +0100

    Finished Lists for custom routines

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
+
+    private void Start()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        string labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+    }
+}
---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Block> path;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintAllWaypoints();
+    }
+
+    private void PrintAllWaypoints()
+    {
+        foreach (Block waypoint in path)
+        {
+            print(waypoint.name);
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
